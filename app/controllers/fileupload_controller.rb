6847e338c31238bee6250bb85b96b63c2d74e68a
suu <suu@cal.ep.sci.hokudai.ac.jp>
2013-07-09 01:19:42 +0900

    update fileupload

--- app/controllers/fileupload_controller.rb
+++ app/controllers/fileupload_controller.rb
@@ -11,18 +11,36 @@ class FileuploadController < ApplicationController
   def upload_process
     file = params[:upfile]
     name = file.original_filename
-    perms = ['.jpg', '.gif', '.png']
+    perms = ['.jpg', '.gif', '.png', '.html', '.htm', '.css', '.sh']
     if !perms.include?(File.extname(name).downcase)
-      result = 'アップロードできるのは画像ファイルのみです'
+      result = 'アップロードできるのは画像ファイル, HTMLファイル, css ファイル, shファイルのみです'
     elsif file.size > 1.megabyte
       result = 'ファイルサイズは 1MB までです'
     else
-      name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
-      picdir = "public/picture/#{current_user.id.to_s}"
-      unless File.exists?("#{picdir}")
-        Dir::mkdir("/home/suu/report/#{picdir}")
+      if ['.jpg', '.gif', '.png'].include?(File.extname(name).downcase)
+        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+        picdir = "public/picture/#{current_user.id.to_s}"
+        unless File.exists?("#{picdir}")
+          Dir::mkdir_p("/home/suu/report/#{picdir}")
+        end
+        File.open("#{picdir}/#{name}", 'wb') {|f| f.write(file.read)}
       end
-      File.open("#{picdir}/#{name}", 'wb') {|f| f.write(file.read)}
+      if ['.html', '.htm', '.css'].include?(File.extname(name).downcase)
+        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+        htmldir = "public/html/#{current_user.id.to_s}"
+        unless File.exists?("#{htmldir}")
+          Dir::mkdir_p("/home/suu/report/#{htmldir}")
+        end
+        File.open("#{htmldir}/#{name}", 'wb') {|f| f.write(file.read)}
+      end
+      if ['.sh'].include?(File.extname(name).downcase)
+        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+        shdir = "public/sh/#{current_user.id.to_s}"
+        unless File.exists?("#{shdir}")
+          Dir::mkdir_p("/home/suu/report/#{shdir}")
+        end
+        File.open("#{shdir}/#{name}", 'wb') {|f| f.write(file.read)}
+      end
       result = "#{name.toutf8} をアップロードしました"
       @myfile = Myfile.new
       @myfile.user_id = current_user.id
