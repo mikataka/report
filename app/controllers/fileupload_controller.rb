bf9b36b0ddc7dc1d1fe8afe19ad708f6bb2a7e2f
suu <suu@jet.ep.sci.hokudai.ac.jp>
2013-07-12 14:10:12 +0900

    fileupload update

--- app/controllers/fileupload_controller.rb
+++ app/controllers/fileupload_controller.rb
@@ -10,46 +10,73 @@ class FileuploadController < ApplicationController
 
   def upload_process
     file = params[:upfile]
-    name = file.original_filename
-    perms = ['.jpg', '.gif', '.png', '.html', '.htm', '.css', '.sh']
-    if !perms.include?(File.extname(name).downcase)
-      result = 'アップロードできるのは画像ファイル, HTMLファイル, css ファイル, shファイルのみです'
-    elsif file.size > 1.megabyte
-      result = 'ファイルサイズは 1MB までです'
-    else
-      if ['.jpg', '.gif', '.png'].include?(File.extname(name).downcase)
-        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
-        picdir = "public/picture/#{current_user.id.to_s}"
-        unless File.exists?("#{picdir}")
-          Dir::mkdir_p("/home/suu/report/#{picdir}")
+    if file.nil?
+      result = 'アップロードするファイルが選択されていません'
+      redirect_to fileupload_path, :notice => "#{result}"
+    elsif
+      name = file.original_filename
+      perms = ['.jpg', '.gif', '.png', '.html', '.htm', '.css', '.sh', '.zip']
+      if !perms.include?(File.extname(name).downcase)
+        result = 'アップロードできるのは画像ファイル, HTMLファイル, css ファイル, shファイル, zip ファイルのみです'
+        redirect_to fileupload_path, :notice => "#{result}"
+      elsif file.size > 1.megabyte
+        result = 'ファイルサイズは 1MB までです'
+        redirect_to fileupload_path, :notice => "#{result}"
+      else
+        if ['.jpg', '.gif', '.png'].include?(File.extname(name).downcase)
+          name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+          picdir = "public/picture/#{current_user.id.to_s}"
+          unless File.exists?("#{picdir}")
+            Dir::mkdir("/home/suu/report/#{picdir}")
+          end
+          File.open("#{picdir}/#{name}", 'wb') {|f| f.write(file.read)}
         end
-        File.open("#{picdir}/#{name}", 'wb') {|f| f.write(file.read)}
-      end
-      if ['.html', '.htm', '.css'].include?(File.extname(name).downcase)
-        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
-        htmldir = "public/html/#{current_user.id.to_s}"
-        unless File.exists?("#{htmldir}")
-          Dir::mkdir_p("/home/suu/report/#{htmldir}")
+        if ['.html', '.htm', '.css'].include?(File.extname(name).downcase)
+          name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+          htmldir = "public/html/#{current_user.id.to_s}"
+          unless File.exists?("#{htmldir}")
+            Dir::mkdir("/home/suu/report/#{htmldir}")
+          end
+          File.open("#{htmldir}/#{name}", 'wb') {|f| f.write(file.read)}
         end
-        File.open("#{htmldir}/#{name}", 'wb') {|f| f.write(file.read)}
-      end
-      if ['.sh'].include?(File.extname(name).downcase)
-        name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
-        shdir = "public/sh/#{current_user.id.to_s}"
-        unless File.exists?("#{shdir}")
-          Dir::mkdir_p("/home/suu/report/#{shdir}")
+        if ['.sh'].include?(File.extname(name).downcase)
+          name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+          shdir = "public/sh/#{current_user.id.to_s}"
+          unless File.exists?("#{shdir}")
+            Dir::mkdir("/home/suu/report/#{shdir}")
+          end
+          File.open("#{shdir}/#{name}", 'wb') {|f| f.write(file.read)}
         end
-        File.open("#{shdir}/#{name}", 'wb') {|f| f.write(file.read)}
+        if ['.zip'].include?(File.extname(name).downcase)
+          name = name.kconv( Kconv::SJIS, Kconv::UTF8 )
+          zipdir = "public/zip/#{current_user.id.to_s}"
+          unless File.exists?("#{zipdir}")
+            Dir::mkdir("/home/suu/report/#{zipdir}")
+          end
+          File.open("#{zipdir}/#{name}", 'wb') {|f| f.write(file.read)}
+        end
+        result = "#{name.toutf8} をアップロードしました"
+        @myfile = Myfile.new
+        @myfile.user_id = current_user.id
+        @myfile.filename = name
+        @myfile.caption = params[:caption].to_s
+        @myfile.save
+        redirect_to user_fileupload_index_path(current_user.id), :notice => "#{result}"
       end
-      result = "#{name.toutf8} をアップロードしました"
-      @myfile = Myfile.new
-      @myfile.user_id = current_user.id
-      @myfile.filename = name
-      @myfile.caption = params[:caption].to_s
-      @myfile.save
     end
-    redirect_to fileupload_path, :notice => "#{result}"
   end
+
+  def destroy
+    @myfile = Myfile.find(params[:id])
+    @myfile.destroy
+
+    respond_to do |format|
+      format.html { redirect_to user_fileupload_index_path(current_user.id) }
+      format.json { head :no_content }
+    end
+  end
+
+
   
   private
   def user_checked 
